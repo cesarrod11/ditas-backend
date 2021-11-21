f21ac23a152f35c58f9e3830378bf8a80024acb2
Cesar Rodrigues <cesar.rodrigues@picpay.com>
2021-11-20 22:43:59 -0300

    refactoring files

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
-import providersRouter from 'dist/modules/appointments/infra/http/routes/providers.routes';
-import usersRouter from 'dist/modules/users/infra/routes/http/users.routes';
-import sessionsRouter from 'dist/modules/users/infra/routes/http/sessions.routes';
-import passwordRouter from 'dist/modules/users/infra/routes/http/password.routes';
-import profileRouter from 'dist/modules/users/infra/routes/http/profile.routes';
-import appointmentsRouter from 'dist/modules/appointments/infra/http/routes/appointments.routes';
+import providersRouter from 'modules/appointments/infra/http/routes/providers.routes';
+import usersRouter from 'modules/users/infra/routes/http/users.routes';
+import sessionsRouter from 'modules/users/infra/routes/http/sessions.routes';
+import passwordRouter from 'modules/users/infra/routes/http/password.routes';
+import profileRouter from 'modules/users/infra/routes/http/profile.routes';
+import appointmentsRouter from 'modules/appointments/infra/http/routes/appointments.routes';
 
 const routes = Router();
 
